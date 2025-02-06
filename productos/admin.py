e603b0326bbfbf387b26fbe5ecaea35cb5613e81
yoshua soto <yoshuasotodev95@gmail.com>
2025-02-06 08:47:46 -0400

    second commit

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Categoria, Producto
+
+# personalizar el administrador
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+
+class ProductoAdmin(admin.ModelAdmin):
+    # Oculta el campo creado_en
+    exclude = ('creado_en',)
+    # Despliga los campos en la lista de productos
+    list_display = ('id', 'nombre', 'stock', 'puntaje', 'creado_en')
+
 
 # Register your models here.
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Producto, ProductoAdmin)
--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    puntaje = models.FloatField()
+    # Cascade: elimina el producto si se elimina la categoria
+    # Protect: lanza un error si se intenta eliminar la categoria
+    # Restric: solo elimina si no existe productos relacionados
+    # SetNull: pone en null la categoria si se elimina
+    # setDefault: pone un valor por defecto si se elimina
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    # auto_now_add: se crea la fecha al crear el registro
+    creado_en = models.DateTimeField(default=timezone.now)
+
+    # Oculta el campo creado_en
+    def __str__(self):
+        return self.nombre
