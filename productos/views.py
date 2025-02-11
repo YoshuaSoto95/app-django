a5fcf8ac21df80fc6393afb1badff25f6fcf3752
yoshua soto <yoshuasotodev95@gmail.com>
2025-02-11 14:30:06 -0400

    Plantillas Agregadas

--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, World!')
+    productos = Producto.objects.all()
+    return render(request, 'index.html', {'productos': productos})
